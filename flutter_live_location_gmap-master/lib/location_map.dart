c6f55cf812c8961238d9747b67c3c16d002875c9
HerooNAB <56840074+HerooNAB@users.noreply.github.com>
2021-04-17 01:43:52 +0700

    update client maps

---
+++ flutter_live_location_gmap-master/lib/location_map.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_live_location_gmap/coffee_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'location_provider.dart';
+
+class LocationMap extends StatefulWidget {
+  @override
+  _LocationMapState createState() => _LocationMapState();
+}
+
+class _LocationMapState extends State<LocationMap> {
+  GoogleMapController _controller;
+
+  List<Marker> allMarkers = [];
+
+  PageController _pageController;
+
+  int prevPage;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    super.initState();
+    Provider.of<LocationProvider>(context, listen: false).initialization();
+
+    coffeeShops.forEach((element) {
+      allMarkers.add(Marker(
+          markerId: MarkerId(element.shopName),
+          draggable: false,
+          infoWindow:
+              InfoWindow(title: element.shopName, snippet: element.address),
+          position: element.locationCoords));
+    });
+    _pageController = PageController(initialPage: 1, viewportFraction: 0.8)
+      ..addListener(_onScroll);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google Map - NAB"),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: googleMapUI(),
+    );
+  }
+
+  Widget googleMapUI() {
+    return Consumer<LocationProvider>(builder: (consumerContext, model, child) {
+      if (model.locationPosition != null) {
+        return Scaffold(
+          body: Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height - 88.0,
+                width: MediaQuery.of(context).size.width,
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                      target: model.locationPosition, zoom: 12.0),
+                  markers: Set.from(allMarkers),
+                  onMapCreated: mapCreated,
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                ),
+              ),
+              Positioned(
+                bottom: 20.0,
+                child: Container(
+                  height: 200.0,
+                  width: MediaQuery.of(context).size.width,
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: coffeeShops.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return _coffeeShopList(index);
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    });
+  }
+
+  _coffeeShopList(index) {
+    return AnimatedBuilder(
+      animation: _pageController,
+      builder: (BuildContext context, Widget widget) {
+        double value = 1;
+        if (_pageController.position.haveDimensions) {
+          value = _pageController.page - index;
+          value = (1 - (value.abs() * 0.3) + 0.06).clamp(0.0, 1.0);
+        }
+        return Center(
+          child: SizedBox(
+            height: Curves.easeInOut.transform(value) * 135.0,
+            width: Curves.easeInOut.transform(value) * 345.0,
+            child: widget,
+          ),
+        );
+      },
+      child: InkWell(
+          onTap: () {},
+          child: Stack(children: [
+            Center(
+                child: Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 20.0,
+                    ),
+                    height: 130.0,
+                    width: 275.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(0.0, 4.0),
+                            blurRadius: 10.0,
+                          ),
+                        ]),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white),
+                        child: Row(children: [
+                          Container(
+                              height: 90.0,
+                              width: 90.0,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10.0),
+                                      topLeft: Radius.circular(10.0)),
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          coffeeShops[index].thumbNail),
+                                      fit: BoxFit.cover))),
+                          SizedBox(width: 5.0),
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  coffeeShops[index].shopName,
+                                  style: TextStyle(
+                                      fontSize: 12.5,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  coffeeShops[index].address,
+                                  style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Container(
+                                  width: 170.0,
+                                  child: Text(
+                                    coffeeShops[index].description,
+                                    style: TextStyle(
+                                        fontSize: 11.0,
+                                        fontWeight: FontWeight.w300),
+                                  ),
+                                )
+                              ])
+                        ]))))
+          ])),
+    );
+  }
+
+  void mapCreated(controller) {
+    setState(() {
+      _controller = controller;
+    });
+  }
+
+  moveCamera() {
+    _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: coffeeShops[_pageController.page.toInt()].locationCoords,
+        zoom: 14.0,
+        bearing: 45.0,
+        tilt: 45.0)));
+  }
+
+  void _onScroll() {
+    if (_pageController.page.toInt() != prevPage) {
+      prevPage = _pageController.page.toInt();
+      moveCamera();
+    }
+  }
+}
